06d738cbf4aeac191ba651328cfa5d4faf63637d
AnalysisBlank <yao_jianxu@qq.com>
2017-11-06 21:00:15 +0800

    增加后台自动更新天气的功能及修改程序图标和名称

--- app/src/main/java/com/example/yao/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/yao/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.yao.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.yao.coolweather.gson.ForeCast;
 import com.example.yao.coolweather.gson.Weather;
+import com.example.yao.coolweather.service.AutoUpdateService;
 import com.example.yao.coolweather.util.HttpUtil;
 import com.example.yao.coolweather.util.Utility;
 
@@ -56,7 +58,7 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_weather);
@@ -150,9 +152,9 @@ public class WeatherActivity extends AppCompatActivity {
         loadBingPic();
     }
 
-    public void  showWeatherInfo(Weather weather) {
+    private void  showWeatherInfo(Weather weather) {
         String cityName = weather.basic.cityName;
-        String updateTime = weather.basic.update.updateTime.split(" ")[1];
+        String updateTime = "更新于" + weather.basic.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature + "℃";
         String weatherInfo = weather.now.more.info;
         titleCity.setText(cityName);
@@ -183,9 +185,11 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
-    public void loadBingPic() {
+    private void loadBingPic() {
         String requestBingPic = "http://guolin.tech/api/bing_pic";
         HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
             @Override
